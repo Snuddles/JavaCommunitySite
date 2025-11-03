df37ac1b5297ff4a984e054fea2d7f96eec530c5
Raulph Peltrop <raulph.peltrop@gmail.com>
2025-11-02 23:04:27 -0500

    remove: Removed old scripts associated with AWS to fix migrate up with azure db

--- migrations/scripts/20251021211000_change_vote_type_from_smallint_to_Integer.sql
+++
@@ -1,9 +0,0 @@
--- //UP
--- Change vote_type (or value) column to integer
-ALTER TABLE public.vote
-ALTER COLUMN value TYPE integer USING value::integer;
-
--- //@UNDO
--- Revert column back to smallint if needed
-ALTER TABLE public.vote
-ALTER COLUMN value TYPE smallint USING value::smallint;
